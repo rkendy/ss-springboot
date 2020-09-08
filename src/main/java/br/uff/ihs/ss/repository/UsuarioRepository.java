cf132577e6146fe6a0dd7cb70d65db22ccf3fc74
Roberto Kendy Sawamura <rkendy@gmail.com>
2020-09-08 19:20:12 -0300

    Add find by login fetch setor

--- src/main/java/br/uff/ihs/ss/repository/UsuarioRepository.java
+++ src/main/java/br/uff/ihs/ss/repository/UsuarioRepository.java
@@ -2,10 +2,14 @@ package br.uff.ihs.ss.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import br.uff.ihs.ss.model.Usuario;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
     Optional<Usuario> findByLogin(String login);
+
+    @Query("select u from Usuario u left join fetch u.setores where u.login = ?1")
+    Optional<Usuario> findByLoginSetores(String login);
 }
--- src/test/java/br/uff/ihs/ss/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/uff/ihs/ss/repository/UsuarioRepositoryTest.java
@@ -3,11 +3,12 @@ package br.uff.ihs.ss.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.util.Optional;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
 import br.uff.ihs.ss.helper.UsuarioTestHelper;
+import br.uff.ihs.ss.model.Setor;
 import br.uff.ihs.ss.model.Usuario;
 
 public class UsuarioRepositoryTest extends CrudRepositoryTest<UsuarioRepository, Usuario> {
@@ -37,6 +38,23 @@ public class UsuarioRepositoryTest extends CrudRepositoryTest<UsuarioRepository,
         assertEquals(u.getNome(), actual.getNome());
         assertEquals(u.getEmail(), actual.getEmail());
         assertEquals(u.getAdmin(), actual.getAdmin());
+    }
+
+    @Test
+    public void givenUsuarioWithSetor_whenFindByLoginSetor_thenSetores() {
+        Usuario u = Usuario.builder().login("login001").nome("Nome").build();
+        Setor s = Setor.builder().codigo("codigo").nome("SetorNome").build();
+        u.setSetores(Arrays.asList(s));
+        repository.save(u);
+
+        Usuario uu = repository.findByLoginSetores(u.getLogin()).get();
+
+        assertEquals(1, uu.getSetores().size());
+        assertEquals(u.getLogin(), uu.getLogin());
+        assertEquals(u.getNome(), uu.getNome());
+        assertEquals(s.getNome(), uu.getSetores().get(0).getNome());
+        assertEquals(s.getCodigo(), uu.getSetores().get(0).getCodigo());
+
     }
 
 }
