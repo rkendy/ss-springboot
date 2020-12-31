d2eb6652f7465ae7dee5a460e24f9962920d3e1f
Roberto Kendy Sawamura <rkendy@gmail.com>
2020-12-31 18:39:29 -0300

    Adjustments in crud generation

--- src/crud-template/ModelController.java
+++ src/crud-template/ModelController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.uff.ihs.ss.service.CrudService;
 import br.uff.ihs.ss.dto.$Model$Dto;
 import br.uff.ihs.ss.model.$Model$;
 import br.uff.ihs.ss.service.$Model$Service;
@@ -14,9 +15,16 @@ public class $Model$Controller extends BaseCrudController<$Model$, $Model$Dto> {
 
     public static final String ENDPOINT = "/api/$model$";
 
+    private $Model$Service service;
+
     @Autowired
     public $Model$Controller($Model$Service service) {
         this.service = service;
+    }
+
+    @Override
+    protected CrudService<$Model$> getService() {
+        return service;
     }
 
     @Override
--- src/crud-template/ModelControllerTest.java
+++ src/crud-template/ModelControllerTest.java
@@ -3,16 +3,27 @@ package br.uff.ihs.ss.controller;
 import java.util.List;
 
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
+import br.uff.ihs.ss.service.CrudService;
 import br.uff.ihs.ss.dto.$Model$Dto;
 import br.uff.ihs.ss.helper.$Model$TestHelper;
 import br.uff.ihs.ss.helper.TestHelper;
 import br.uff.ihs.ss.model.$Model$;
+import br.uff.ihs.ss.service.$Model$Service;
 
 public class $Model$ControllerTest extends CrudControllerTest<$Model$, $Model$Dto> {
 
     @InjectMocks
     $Model$Controller $model$Controller;
+
+    @Mock
+    private $Model$Service service;
+
+    @Override
+    protected CrudService<$Model$> getService() {
+        return service;
+    }
 
     private TestHelper<$Model$> helper = new $Model$TestHelper();
 
